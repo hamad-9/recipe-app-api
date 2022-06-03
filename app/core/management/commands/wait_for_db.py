1fb71930b7c60ed0a639c7c7a46cf3e9de74c103
Hamad Numan <hamadnuman.dev@gmail.com>
2022-06-03 17:04:02 +0300

    configured docker and cheacks for wait_for_db 2

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -11,16 +11,17 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     """Django command to wait for database."""
+
     def handle(self, *args, **options):
-        """entry point for command"""
+        """Entry point for command."""
         self.stdout.write('waiting for database...')
-        dp_up = False
-        while dp_up is False:
+        db_up = False
+        while db_up is False:
             try:
                 self.check(databases=['default'])
-                dp_up = True
-            except(Psycopg2OpError, OperationalError):
-                self.stdout.write('Database Unavailable, waiting 1 second...')
+                db_up = True
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database available!'))
